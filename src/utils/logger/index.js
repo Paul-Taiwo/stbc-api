db2cf55ca6b0746c003e51bc7681baf8389067dc
Paul Taiwo <ayopaulot@gmail.com>
2020-07-17 16:01:23 +0100

    fix logger

--- src/utils/logger/index.js
+++ src/utils/logger/index.js
@@ -1,21 +1,21 @@
-import winston from 'winston';
-import winstonPapertrail from 'winston-papertrail';
+import winston from "winston";
+import winstonPapertrail from "winston-papertrail";
 
-import config from '../../config';
+import config from "../../config";
 
 let logger;
 
-if (config.env == 'test' || config.env == 'local' || config.env == 'development') {
-	logger = console;
+if (config.env == "test" || config.env == "local" || config.env == "development") {
+  logger = console;
 } else {
-	// const papertrailTransport = new winston.transports.Papertrail({
-	// 	host: config.logger.host,
-	// 	port: config.logger.port,
-	// });
+  const papertrailTransport = new winston.transports.Papertrail({
+    host: config.logger.host,
+    port: config.logger.port,
+  });
 
-	// logger = new winston.Logger({
-	// 	transports: [papertrailTransport],
-	// });
+  logger = new winston.Logger({
+    transports: [papertrailTransport],
+  });
 }
 
 export default logger;
