e01c56bd1ec52f266a3520f6bf9a88eaca46c309
Paul Taiwo <ayopaulot@gmail.com>
2020-07-17 17:02:41 +0100

    remove console.log()

--- src/routes/main.js
+++ src/routes/main.js
@@ -3,8 +3,6 @@ import express from "express";
 const Route = express.Router();
 
 Route.get("/", (req, res) => {
-  console.log("Heyyy", req.body);
-
   return res.status(200).json({
     status: 200,
     message: "Welcome to STBC API",
